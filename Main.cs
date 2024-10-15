b6205354345c8e197ddf072127381ed699c1c303
Daniel Corral <167433136+Korra-5@users.noreply.github.com>
2024-10-15 20:33:34 +0200

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -1,4 +1,4 @@
-﻿namespace Proyecto1;
+namespace Proyecto1;
 
  class MainClass
 {
@@ -6,10 +6,10 @@
     {
         var character = new Character(50,"Heroe",100,15,10);
         var minion = new Minion("Minion1", 30, 30, 4, 4);
-        var axe = new Weapon (){ Damage = 7 };
-        var sword = new Weapon() { Damage = 5 };
-        var helmet = new Protection(){ Armor = 4 };
-        var shield = new Protection(){ Armor = 3 };
+        var axe = new Axe(){ Damage = 7 };
+        var sword = new Sword() { Damage = 5 };
+        var helmet = new Helmet(){ Armor = 4 };
+        var shield = new Shield(){ Armor = 3 };
         character.AddItem(axe);
         character.AddItem(sword);
         character.AddItem(helmet);
@@ -20,12 +20,17 @@
         Console.WriteLine(character);
         Console.WriteLine(minion);
         
+        character.RemoveItem(axe);
         character.ReceiveDamage(20);
+        Console.WriteLine(character);
+        
+        character.ReceiveDamage(2);
         Console.WriteLine(character);
         
         minion.ReceiveDamageMinion(20);
         Console.WriteLine(minion);
         
+        character.RemoveItem(helmet);
         character.Heal(30);
         Console.WriteLine(character);
         
